74aae9bcfd0200625c8488c5cdaac56adb153695
Nicolaj Nielsen <nicolaj.niels1@gmail.com>
2023-09-18 11:31:18 +0200

    refactor(tests): refactor tests

--- tests/test_myers.py
+++ tests/test_myers.py
@@ -1,151 +1,109 @@
-from typing import Literal, NamedTuple
-
+# vim: foldlevel=0
 import pytest
-from numpy import who
-from shapely import LineString, Point
 
-from geomdiff.myers import Diff, _diff, _find_middle_snake, diff_clean
+import geomdiff.myers as geomdiff
+
+# TODO: Use fixtures/scenarios
 
 # TESTS for _find_middle_snake
 
 
-def test_find_middle_snake_empty_to_empty():
-    want = (0, 0, 0, 0, 0)
-    got = _find_middle_snake([], [], 0, 0)
-    assert got == want
+MiddleSnakeScenario = tuple[
+    str,
+    tuple[
+        geomdiff.PointSequence, geomdiff.PointSequence, tuple[int, int, int, int, int]
+    ],
+]
+scenarios: list[MiddleSnakeScenario] = [
+    ("nil -> nil", ([], [], (0, 0, 0, 0, 0))),
+    ("nil -> a", ([], [(0, 0)], (1, 0, 1, 0, 1))),
+    ("a -> nil", ([(0, 0)], [], (1, 1, 0, 1, 0))),
+    ("a -> a", ([(0, 0)], [(0, 0)], (0, 0, 0, 1, 1))),
+    ("a -> b", ([(0, 0)], [(1, 1)], (2, 1, 0, 1, 0))),
+    ("nil -> ab", ([], [(0, 0), (1, 1)], (2, 0, 1, 0, 1))),
+    ("nil -> abc", ([], [(0, 0), (1, 1), (2, 2)], (3, 0, 2, 0, 2))),
+    ("ab -> nil", ([(0, 0), (1, 1)], [], (2, 1, 0, 1, 0))),
+    ("abc -> nil", ([(0, 0), (1, 1), (2, 2)], [], (3, 2, 0, 2, 0))),
+    (
+        "abc -> abc",
+        ([(0, 0), (1, 1), (2, 2)], [(0, 0), (1, 1), (2, 2)], (0, 0, 0, 3, 3)),
+    ),
+    (
+        "abcd -> ebcd",
+        (
+            [(0, 0), (1, 1), (2, 2), (3, 3)],
+            [(4, 4), (1, 1), (2, 2), (3, 3)],
+            (2, 1, 0, 1, 0),
+        ),
+    ),
+    (
+        "abcd -> aecd",
+        (
+            [(0, 0), (1, 1), (2, 2), (3, 3)],
+            [(0, 0), (4, 4), (2, 2), (3, 3)],
+            (2, 2, 1, 2, 1),
+        ),
+    ),
+    (
+        "abcd -> aefd",
+        (
+            [(0, 0), (1, 1), (2, 2), (3, 3)],
+            [(0, 0), (4, 4), (5, 5), (3, 3)],
+            (4, 3, 1, 3, 1),
+        ),
+    ),
+    (
+        "abcd -> bc",
+        ([(0, 0), (1, 1), (2, 2), (3, 3)], [(1, 1), (2, 2)], (2, 1, 0, 3, 2)),
+    ),
+    (
+        "abcde -> bc",
+        ([(0, 0), (1, 1), (2, 2), (3, 3), (4, 4)], [(1, 1), (2, 2)], (3, 4, 2, 4, 2)),
+    ),
+]
 
 
-def test_find_middle_snake_empty_to_point():
-    want = (1, 0, 1, 0, 1)
-    got = _find_middle_snake([], [(0, 0)], 0, 1)
-    assert got == want
+def idfn(val: MiddleSnakeScenario):
+    return val[0]
 
 
-def test_find_middle_snake_point_to_empty():
-    want = (1, 1, 0, 1, 0)
-    got = _find_middle_snake([(0, 0)], [], 1, 0)
-    assert got == want
+@pytest.mark.parametrize("scenario", scenarios, ids=idfn)
+def test_find_middle_snake(scenario: MiddleSnakeScenario):
+    data = scenario[1]
+    a = data[0]
+    b = data[1]
+    want = data[2]
+    N = len(a)
+    M = len(b)
+    got = geomdiff._find_middle_snake(a, b, N, M)
+    assert want == got
 
-
-def test_find_middle_snake_point_a_to_point_a():
-    seq1 = [(0, 0)]
-    seq2 = [(0, 0)]
-    want = (0, 0, 0, 1, 1)
-    got = _find_middle_snake(seq1, seq2, 1, 1)
-    assert got == want
-
-
-def test_find_middle_snake_point_a_to_point_b():
-    seq1 = [(0, 0)]
-    seq2 = [(1, 1)]
-    want = (2, 1, 0, 1, 0)
-    got = _find_middle_snake(seq1, seq2, 1, 1)
-    assert got == want
-
-
-def test_find_middle_snake_empty_to_linestring_even():
-    seq1 = []
-    seq2 = [(0, 0), (1, 1)]
-    want = (2, 0, 1, 0, 1)
-    got = _find_middle_snake(seq1, seq2, 0, 2)
-    assert got == want
-
-
-def test_find_middle_snake_empty_to_linestring_odd():
-    seq1 = []
-    seq2 = [(0, 0), (1, 1), (2, 2)]
-    want = (3, 0, 2, 0, 2)
-    got = _find_middle_snake(seq1, seq2, 0, 3)
-    assert got == want
-
-
-def test_find_middle_snake_linestring_to_empty_even():
-    seq1 = [(0, 0), (1, 1)]
-    seq2 = []
-    want = (2, 1, 0, 1, 0)
-    got = _find_middle_snake(seq1, seq2, 2, 0)
-    assert got == want
-
-
-def test_find_middle_snake_linestring_to_empty_odd():
-    seq1 = [(0, 0), (1, 1), (2, 2)]
-    seq2 = []
-    want = (3, 2, 0, 2, 0)
-    got = _find_middle_snake(seq1, seq2, 3, 0)
-    assert got == want
-
-
-def test_find_middle_snake_linestring_to_equal():
-    seq1 = [(0, 0), (1, 1), (2, 2)]
-    seq2 = [(0, 0), (1, 1), (2, 2)]
-    want = (0, 0, 0, 3, 3)
-    got = _find_middle_snake(seq1, seq2, 3, 3)
-    assert got == want
-
-
-def test_find_middle_snake_linestring_change_first():
-    seq1 = [(0, 0), (1, 1), (2, 2), (3, 3)]
-    seq2 = [(4, 4), (1, 1), (2, 2), (3, 3)]
-    want = (2, 1, 0, 1, 0)
-    got = _find_middle_snake(seq1, seq2, 4, 4)
-    assert got == want
-
-
-def test_find_middle_snake_linestring_change_second():
-    seq1 = [(0, 0), (1, 1), (2, 2), (3, 3)]
-    seq2 = [(0, 0), (4, 4), (2, 2), (3, 3)]
-    want = (2, 2, 1, 2, 1)
-    got = _find_middle_snake(seq1, seq2, 4, 4)
-    assert got == want
-
-
-def test_find_middle_snake_linestring_change_middle_two():
-    seq1 = [(0, 0), (1, 1), (2, 2), (3, 3)]
-    seq2 = [(0, 0), (4, 4), (5, 5), (3, 3)]
-    want = (4, 1, 1, 3, 3)
-    got = _find_middle_snake(seq1, seq2, 4, 4)
-
-
-def test_find_middle_snake_delta_even():
-    seq1 = ["a", "b", "c", "d"]
-    seq2 = ["b", "c"]
-    want = (2, 1, 0, 3, 2)
-    got = _find_middle_snake(seq1, seq2, 4, 2)
-    assert got == want
-
-
-def test_find_middle_snake_delta_odd():
-    seq1 = [(0, 0), (1, 1), (2, 2), (3, 3), (4, 4)]
-    seq2 = [(1, 1), (2, 2)]
-    want = (3, 4, 2, 4, 2)
-    got = _find_middle_snake(seq1, seq2, 5, 2)
-    assert got == want
-
+# TODO: Use scenarios
 
 def test_diff_empty_to_empty():
     want = []
-    got = _diff([], [], 0, 0)
+    got = geomdiff._shortest_edit_script([], [], 0, 0)
     assert got == want
 
 
 def test_diff_empty_to_point():
     p = (0, 0)
     want = [(-1, "insert", p)]
-    got = _diff([], [p], 0, 0)
+    got = geomdiff._shortest_edit_script([], [p], 0, 0)
     assert got == want
 
 
 def test_diff_point_to_empty():
     p = (0, 0)
     want = [(0, "delete")]
-    got = _diff([p], [], 0, 0)
+    got = geomdiff._shortest_edit_script([p], [], 0, 0)
     assert got == want
 
 
 def test_diff_point_to_equal():
     p = (0, 0)
     want = []
-    got = _diff([p], [p], 0, 0)
+    got = geomdiff._shortest_edit_script([p], [p], 0, 0)
     assert got == want
 
 
@@ -153,21 +111,21 @@ def test_diff_point_a_to_point_b():
     a = (0, 0)
     b = (1, 1)
     want = [(0, "delete"), (0, "insert", b)]
-    got = _diff([a], [b], 0, 0)
+    got = geomdiff._shortest_edit_script([a], [b], 0, 0)
     assert got == want
 
 
 def test_diff_empty_to_linestring():
     a = [(0, 0), (1, 1)]
     want = [(-1, "insert", (0, 0)), (-1, "insert", (1, 1))]
-    got = _diff([], a, 0, 0)
+    got = geomdiff._shortest_edit_script([], a, 0, 0)
     assert got == want
 
 
 def test_diff_linestring_to_empty():
     a = [(0, 0), (1, 1)]
     want = [(0, "delete"), (1, "delete")]
-    got = _diff(a, [], 0, 0)
+    got = geomdiff._shortest_edit_script(a, [], 0, 0)
     assert got == want
 
 
@@ -175,7 +133,7 @@ def test_diff_linestring_appendleft_2_string():
     a = [(1, 1)]
     b = [(0, 0), (1, 1)]
     want = [(-1, "insert", (0, 0))]
-    got = _diff(a, b, 0, 0)
+    got = geomdiff._shortest_edit_script(a, b, 0, 0)
     assert got == want
 
 
@@ -183,7 +141,7 @@ def test_diff_linestring_popleft_2_string():
     a = [(1, 1), (2, 2)]
     b = [(2, 2)]
     want = [(0, "delete")]
-    got = _diff(a, b, 0, 0)
+    got = geomdiff._shortest_edit_script(a, b, 0, 0)
     assert got == want
 
 
@@ -191,7 +149,7 @@ def test_diff_linestring_popnpushleft_2_string():  # change first
     a = [(1, 1), (2, 2)]
     b = [(3, 3), (2, 2)]
     want = [(0, "delete"), (0, "insert", (3, 3))]
-    got = _diff(a, b, 0, 0)
+    got = geomdiff._shortest_edit_script(a, b, 0, 0)
     assert got == want
 
 
@@ -199,7 +157,7 @@ def test_diff_linestring_pop_2_string():
     a = [(1, 1), (2, 2)]
     b = [(1, 1)]
     want = [(1, "delete")]
-    got = _diff(a, b, 0, 0)
+    got = geomdiff._shortest_edit_script(a, b, 0, 0)
     assert got == want
 
 
@@ -207,7 +165,7 @@ def test_diff_linestring_push_2_string():
     a = [(1, 1)]
     b = [(1, 1), (2, 2)]
     want = [(0, "insert", (2, 2))]
-    got = _diff(a, b, 0, 0)
+    got = geomdiff._shortest_edit_script(a, b, 0, 0)
     assert got == want
 
 
@@ -215,7 +173,7 @@ def test_diff_linestring_poppush_2_string():
     a = [(1, 1), (2, 2)]
     b = [(1, 1), (3, 3)]
     want = [(1, "delete"), (1, "insert", (3, 3))]
-    got = _diff(a, b, 0, 0)
+    got = geomdiff._shortest_edit_script(a, b, 0, 0)
     assert got == want
 
 
@@ -223,7 +181,7 @@ def test_diff_linestring_pushleft_3_string():
     a = [(1, 1), (2, 2)]
     b = [(0, 0), (1, 1), (2, 2)]
     want = [(-1, "insert", (0, 0))]
-    got = _diff(a, b, 0, 0)
+    got = geomdiff._shortest_edit_script(a, b, 0, 0)
     assert got == want
 
 
@@ -231,7 +189,7 @@ def test_diff_linestring_popleft_3_string():
     a = [(1, 1), (2, 2), (3, 3)]
     b = [(2, 2), (3, 3)]
     want = [(0, "delete")]
-    got = _diff(a, b, 0, 0)
+    got = geomdiff._shortest_edit_script(a, b, 0, 0)
     assert got == want
 
 
@@ -239,7 +197,7 @@ def test_diff_linestring_popnpushleft_3_string():
     a = [(1, 1), (2, 2), (3, 3)]
     b = [(4, 4), (2, 2), (3, 3)]
     want = [(0, "delete"), (0, "insert", (4, 4))]
-    got = _diff(a, b, 0, 0)
+    got = geomdiff._shortest_edit_script(a, b, 0, 0)
     assert got == want
 
 
@@ -247,7 +205,7 @@ def test_diff_linestring_popmiddle_3_string():
     a = [(1, 1), (2, 2), (3, 3)]
     b = [(1, 1), (3, 3)]
     want = [(1, "delete")]
-    got = _diff(a, b, 0, 0)
+    got = geomdiff._shortest_edit_script(a, b, 0, 0)
     assert got == want
 
 
@@ -255,7 +213,7 @@ def test_diff_linestring_pushmiddle_3_string():
     a = [(1, 1), (3, 3)]
     b = [(1, 1), (2, 2), (3, 3)]
     want = [(0, "insert", (2, 2))]
-    got = _diff(a, b, 0, 0)
+    got = geomdiff._shortest_edit_script(a, b, 0, 0)
     assert got == want
 
 
@@ -263,7 +221,7 @@ def test_diff_linestring_popnpushmiddle_3_string():
     a = [(1, 1), (2, 2), (3, 3)]
     b = [(1, 1), (4, 4), (3, 3)]
     want = [(1, "delete"), (1, "insert", (4, 4))]
-    got = _diff(a, b, 0, 0)
+    got = geomdiff._shortest_edit_script(a, b, 0, 0)
     assert got == want
 
 
@@ -271,7 +229,7 @@ def test_diff_linestring_pop_3_string():
     a = [(1, 1), (2, 2), (3, 3)]
     b = [(1, 1), (2, 2)]
     want = [(2, "delete")]
-    got = _diff(a, b, 0, 0)
+    got = geomdiff._shortest_edit_script(a, b, 0, 0)
     assert got == want
 
 
@@ -279,7 +237,7 @@ def test_diff_linestring_push_3_string():
     a = [(1, 1), (2, 2)]
     b = [(1, 1), (2, 2), (3, 3)]
     want = [(1, "insert", (3, 3))]
-    got = _diff(a, b, 0, 0)
+    got = geomdiff._shortest_edit_script(a, b, 0, 0)
     assert got == want
 
 
@@ -287,7 +245,15 @@ def test_diff_linestring_popnpush_3_string():
     a = [(1, 1), (2, 2), (3, 3)]
     b = [(1, 1), (2, 2), (4, 4)]
     want = [(2, "delete"), (2, "insert", (4, 4))]
-    got = _diff(a, b, 0, 0)
+    got = geomdiff._shortest_edit_script(a, b, 0, 0)
+    assert got == want
+
+
+def test_diff_linestring_popnchange_2_string():
+    a = [(1, 1), (2, 2)]
+    b = [(3, 3)]
+    want = [(0, "delete"), (0, "insert", (3, 3)), (1, "delete")]
+    got = geomdiff._shortest_edit_script(a, b, 0, 0)
     assert got == want
 
 
@@ -302,7 +268,7 @@ def test_diff_linestring_example_from_article():
         (5, "delete"),
         (6, "insert", (3, 3)),
     ]
-    got = _diff(a, b, 0, 0)
+    got = geomdiff._shortest_edit_script(a, b, 0, 0)
     assert got == want
 
 
@@ -310,7 +276,7 @@ def test_diff_linestring_example1():
     a = [(1, 1), (2, 2), (3, 3), (4, 4)]
     b = [(2, 2), (3, 3)]
     want = [(0, "delete"), (3, "delete")]
-    got = _diff(a, b, 0, 0)
+    got = geomdiff._shortest_edit_script(a, b, 0, 0)
     assert got == want
 
 
@@ -324,7 +290,7 @@ def test_diff_linestring_example2():
         (1, "delete"),
         (4, "delete"),
     ]
-    got = _diff(a, b, 0, 0)
+    got = geomdiff._shortest_edit_script(a, b, 0, 0)
     assert got == want
 
 
@@ -337,7 +303,5 @@ def test_diff_linestring_example3():
         (1, "insert", (9, 9)),
         (3, "delete"),
     ]
-    got = _diff(a, b, 0, 0)
+    got = geomdiff._shortest_edit_script(a, b, 0, 0)
     assert got == want
-
-
