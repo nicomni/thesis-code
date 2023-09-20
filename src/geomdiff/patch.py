4a350e3a017d6a951d5e2dcf1c1e94bafb6bbcfb
Nicolaj Nielsen <nicolaj.niels1@gmail.com>
2023-09-20 10:57:17 +0200

    feat: apply a patch to a point sequence

---
+++ src/geomdiff/patch.py
@@ -0,0 +1,58 @@
+from collections import deque
+from typing import Deque
+
+from geomdiff.types import (
+    Patch,
+    Point,
+    PointSequence,
+    is_change_command,
+    is_delete_command,
+    is_insert_command,
+)
+
+
+def apply_patch(patch: Patch, points: PointSequence) -> PointSequence:
+    """Add a patch to a point sequence
+    Parameters
+    ----------
+    point_sequence : PointSequence
+        Sequence of points
+    patch : Patch
+        Sequence of patch commands
+    Returns
+    -------
+    PointSequence
+        The sequence of points after applying the patch
+    """
+    N = len(points)
+    idxMap: dict[int, int] = {}
+    result: Deque[Point] = deque(points)
+    # Reverse the patch
+    for _, cmd in enumerate(reversed(patch)):
+        i = cmd[0]
+        if is_insert_command(cmd):
+            # Insert cmd to result, after index i:
+            result.insert(i + 1, cmd[2])
+        if is_delete_command(cmd):
+            del result[i]
+        if is_change_command(cmd):
+            # Change cmd to result at index i:
+            diff = cmd[2]
+            result[i] = add_difference(result[i], diff)
+    return list(result)
+
+
+def add_difference(point: Point, diff: Point) -> Point:
+    """Add a difference to a point
+    Parameters
+    ----------
+    point : Point
+        A point
+    diff : Point
+        A difference
+    Returns
+    -------
+    Point
+        The point after adding the difference
+    """
+    return (point[0] + diff[0], point[1] + diff[1])
---
+++ tests/test_patch.py
@@ -0,0 +1,50 @@
+import pytest
+
+from geomdiff.patch import apply_patch
+from geomdiff.types import Patch, PointSequence
+
+Scenario = tuple[
+    str, tuple[PointSequence, Patch, PointSequence]
+]  # (id, (point_sequence, patch, expected_result))
+testdata: list[Scenario] = [
+    ("nil -> nil", ([], [], [])),
+    ("nil -> a", ([], [(-1, "insert", (1, 1))], [(1, 1)])),
+    (
+        "nil -> ab",
+        ([], [(-1, "insert", (1, 1)), (0, "insert", (2, 2))], [(1, 1), (2, 2)]),
+    ),
+    ("a -> ab", ([(1, 1)], [(0, "insert", (2, 2))], [(1, 1), (2, 2)])),
+    ("b -> ab", ([(2, 2)], [(-1, "insert", (1, 1))], [(1, 1), (2, 2)])),
+    (
+        "ac -> abc",
+        ([(1, 1), (3, 3)], [(0, "insert", (2, 2))], [(1, 1), (2, 2), (3, 3)]),
+    ),
+    ("a -> nil", ([(1, 1)], [(0, "delete")], [])),
+    ("ab -> nil", ([(1, 1), (2, 2)], [(0, "delete"), (1, "delete")], [])),
+    ("abc -> ac", ([(1, 1), (2, 2), (3, 3)], [(1, "delete")], [(1, 1), (3, 3)])),
+    ("a -> b", ([(1, 1)], [(0, "change", (1, 1))], [(2, 2)])),
+    (
+        "abc -> adc",
+        ([(1, 1), (2, 2), (3, 3)], [(1, "change", (2, 2))], [(1, 1), (4, 4), (3, 3)]),
+    ),
+    ("ab -> ac", ([(1, 1), (2, 2)], [(1, "change", (1, 1))], [(1, 1), (3, 3)])),
+    (
+        "ab -> cd",
+        (
+            [(1, 1), (2, 2)],
+            [(0, "delete"), (1, "change", (1, 1)), (1, "insert", (4, 4))],
+            [(3, 3), (4, 4)],
+        ),
+    ),
+]
+
+
+def idfn(val: Scenario):
+    return val[0]
+
+
+@pytest.mark.parametrize("scenario", testdata, ids=idfn)
+def test_applyPatch(scenario, request):
+    points, patch, want = scenario[1]
+    got = apply_patch(patch, points)
+    assert want == got
