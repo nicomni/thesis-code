3b459a11f74f2d917df558740b2250b67b7529d0
Nicolaj Magga Nielsen <nicolaj.niels1@gmail.com>
2024-02-22 20:39:48 +0100

    chore: refactor test

--- tests/geomdiff/test_shortest_edit_script.py
+++ tests/geomdiff/test_shortest_edit_script.py
@@ -1,232 +1,129 @@
 # vim: foldlevel=0
 
+import pytest
+
 from thesis.geodiff import geodiff
+from thesis.geodiff.types import EditScript, PointSequence
 
 # TODO: Use scenarios
 
+TestId = str
+Input = tuple[PointSequence, PointSequence]
+Expected = EditScript
 
-def test_ses_empty_to_empty():
-    want = []
-    got = geodiff._shortest_edit_script([], [], 0, 0)
-    assert got == want
+Scenario = tuple[TestId, Input, Expected]
 
+Scenarios = list[Scenario]
 
-def test_ses_empty_to_point():
-    p = (0, 0)
-    want = [(-1, "insert", p)]
-    got = geodiff._shortest_edit_script([], [p], 0, 0)
-    assert got == want
-
-
-def test_ses_point_to_empty():
-    p = (0, 0)
-    want = [(0, "delete")]
-    got = geodiff._shortest_edit_script([p], [], 0, 0)
-    assert got == want
-
-
-def test_ses_point_to_equal():
-    p = (0, 0)
-    want = []
-    got = geodiff._shortest_edit_script([p], [p], 0, 0)
-    assert got == want
-
-
-def test_ses_point_a_to_point_b():
-    a = (0, 0)
-    b = (1, 1)
-    want = [(0, "delete"), (0, "insert", b)]
-    got = geodiff._shortest_edit_script([a], [b], 0, 0)
-    assert got == want
-
-
-def test_ses_empty_to_linestring():
-    a = [(0, 0), (1, 1)]
-    want = [(-1, "insert", (0, 0)), (-1, "insert", (1, 1))]
-    got = geodiff._shortest_edit_script([], a, 0, 0)
-    assert got == want
-
-
-def test_ses_linestring_to_empty():
-    a = [(0, 0), (1, 1)]
-    want = [(0, "delete"), (1, "delete")]
-    got = geodiff._shortest_edit_script(a, [], 0, 0)
-    assert got == want
-
-
-def test_ses_linestring_appendleft_2_string():
-    a = [(1, 1)]
-    b = [(0, 0), (1, 1)]
-    want = [(-1, "insert", (0, 0))]
-    got = geodiff._shortest_edit_script(a, b, 0, 0)
-    assert got == want
-
-
-def test_ses_linestring_popleft_2_string():
-    a = [(1, 1), (2, 2)]
-    b = [(2, 2)]
-    want = [(0, "delete")]
-    got = geodiff._shortest_edit_script(a, b, 0, 0)
-    assert got == want
-
-
-def test_ses_linestring_popnpushleft_2_string():  # change first
-    a = [(1, 1), (2, 2)]
-    b = [(3, 3), (2, 2)]
-    want = [(0, "delete"), (0, "insert", (3, 3))]
-    got = geodiff._shortest_edit_script(a, b, 0, 0)
-    assert got == want
-
-
-def test_ses_linestring_pop_2_string():
-    a = [(1, 1), (2, 2)]
-    b = [(1, 1)]
-    want = [(1, "delete")]
-    got = geodiff._shortest_edit_script(a, b, 0, 0)
-    assert got == want
-
-
-def test_ses_linestring_push_2_string():
-    a = [(1, 1)]
-    b = [(1, 1), (2, 2)]
-    want = [(0, "insert", (2, 2))]
-    got = geodiff._shortest_edit_script(a, b, 0, 0)
-    assert got == want
-
-
-def test_ses_linestring_poppush_2_string():
-    a = [(1, 1), (2, 2)]
-    b = [(1, 1), (3, 3)]
-    want = [(1, "delete"), (1, "insert", (3, 3))]
-    got = geodiff._shortest_edit_script(a, b, 0, 0)
-    assert got == want
-
-
-def test_ses_linestring_pushleft_3_string():
-    a = [(1, 1), (2, 2)]
-    b = [(0, 0), (1, 1), (2, 2)]
-    want = [(-1, "insert", (0, 0))]
-    got = geodiff._shortest_edit_script(a, b, 0, 0)
-    assert got == want
-
-
-def test_ses_linestring_popleft_3_string():
-    a = [(1, 1), (2, 2), (3, 3)]
-    b = [(2, 2), (3, 3)]
-    want = [(0, "delete")]
-    got = geodiff._shortest_edit_script(a, b, 0, 0)
-    assert got == want
+scenarios: Scenarios = [
+    ("nil -> nil", ([], []), []),
+    ("nil -> a", ([], [(0, 0)]), [(-1, "insert", (0, 0))]),
+    ("a -> nil", ([(0, 0)], []), [(0, "delete")]),
+    ("a -> a", ([(0, 0)], [(0, 0)]), []),
+    ("a -> b", ([(0, 0)], [(1, 1)]), [(0, "delete"), (0, "insert", (1, 1))]),
+    (
+        "nil -> ab",
+        ([], [(0, 0), (1, 1)]),
+        [(-1, "insert", (0, 0)), (-1, "insert", (1, 1))],
+    ),
+    ("ab -> nil", ([(0, 0), (1, 1)], []), [(0, "delete"), (1, "delete")]),
+    ("b -> ab", ([(1, 1)], [(0, 0), (1, 1)]), [(-1, "insert", (0, 0))]),
+    ("ab -> b", ([(1, 1), (2, 2)], [(2, 2)]), [(0, "delete")]),
+    (
+        "ab -> cb",
+        ([(1, 1), (2, 2)], [(3, 3), (2, 2)]),
+        [(0, "delete"), (0, "insert", (3, 3))],
+    ),
+    ("ab -> a", ([(1, 1), (2, 2)], [(1, 1)]), [(1, "delete")]),
+    ("a -> ab", ([(1, 1)], [(1, 1), (2, 2)]), [(0, "insert", (2, 2))]),
+    (
+        "ab -> ac",
+        ([(1, 1), (2, 2)], [(1, 1), (3, 3)]),
+        [(1, "delete"), (1, "insert", (3, 3))],
+    ),
+    (
+        "bc -> abc",
+        ([(1, 1), (2, 2)], [(0, 0), (1, 1), (2, 2)]),
+        [(-1, "insert", (0, 0))],
+    ),
+    ("abc -> bc", ([(0, 0), (1, 1), (2, 2)], [(1, 1), (2, 2)]), [(0, "delete")]),
+    (
+        "abc -> dbc",
+        ([(0, 0), (1, 1), (2, 2)], [(3, 3), (1, 1), (2, 2)]),
+        [(0, "delete"), (0, "insert", (3, 3))],
+    ),
+    ("abc -> ac", ([(0, 0), (1, 1), (2, 2)], [(0, 0), (2, 2)]), [(1, "delete")]),
+    (
+        "ac -> abc",
+        ([(1, 1), (3, 3)], [(1, 1), (2, 2), (3, 3)]),
+        [(0, "insert", (2, 2))],
+    ),
+    (
+        "abc -> adc",
+        ([(1, 1), (2, 2), (3, 3)], [(1, 1), (4, 4), (3, 3)]),
+        [(1, "delete"), (1, "insert", (4, 4))],
+    ),
+    ("abc -> ab", ([(1, 1), (2, 2), (3, 3)], [(1, 1), (2, 2)]), [(2, "delete")]),
+    (
+        "ab -> abc",
+        ([(1, 1), (2, 2)], [(1, 1), (2, 2), (3, 3)]),
+        [(1, "insert", (3, 3))],
+    ),
+    (
+        "abc -> abd",
+        ([(1, 1), (2, 2), (3, 3)], [(1, 1), (2, 2), (4, 4)]),
+        [(2, "delete"), (2, "insert", (4, 4))],
+    ),
+    (
+        "ab -> c",
+        ([(1, 1), (2, 2)], [(3, 3)]),
+        [(0, "delete"), (0, "insert", (3, 3)), (1, "delete")],
+    ),
+    (
+        "abcabba -> cbabac",
+        (
+            [(1, 1), (2, 2), (3, 3), (1, 1), (2, 2), (2, 2), (1, 1)],
+            [(3, 3), (2, 2), (1, 1), (2, 2), (1, 1), (3, 3)],
+        ),
+        [
+            (0, "delete"),
+            (0, "insert", (3, 3)),
+            (2, "delete"),
+            (5, "delete"),
+            (6, "insert", (3, 3)),
+        ],
+    ),
+    (
+        "abcd -> bc",
+        ([(1, 1), (2, 2), (3, 3), (4, 4)], [(2, 2), (3, 3)]),
+        [(0, "delete"), (3, "delete")],
+    ),
+    (
+        "abcde -> fgcd",
+        ([(1, 1), (2, 2), (3, 3), (4, 4), (5, 5)], [(6, 6), (7, 7), (3, 3), (4, 4)]),
+        [
+            (-1, "insert", (6, 6)),
+            (-1, "insert", (7, 7)),
+            (0, "delete"),
+            (1, "delete"),
+            (4, "delete"),
+        ],
+    ),
+    (
+        "abcd -> cefc",
+        ([(1, 1), (2, 2), (3, 3), (4, 4)], [(2, 2), (8, 8), (9, 9), (3, 3)]),
+        [(0, "delete"), (1, "insert", (8, 8)), (1, "insert", (9, 9)), (3, "delete")],
+    ),
+]
 
 
-def test_ses_linestring_popnpushleft_3_string():
-    a = [(1, 1), (2, 2), (3, 3)]
-    b = [(4, 4), (2, 2), (3, 3)]
-    want = [(0, "delete"), (0, "insert", (4, 4))]
-    got = geodiff._shortest_edit_script(a, b, 0, 0)
-    assert got == want
+def idfn(val: Scenario):
+    return val[0]
 
 
-def test_ses_linestring_popmiddle_3_string():
-    a = [(1, 1), (2, 2), (3, 3)]
-    b = [(1, 1), (3, 3)]
-    want = [(1, "delete")]
-    got = geodiff._shortest_edit_script(a, b, 0, 0)
-    assert got == want
-
-
-def test_ses_linestring_pushmiddle_3_string():
-    a = [(1, 1), (3, 3)]
-    b = [(1, 1), (2, 2), (3, 3)]
-    want = [(0, "insert", (2, 2))]
-    got = geodiff._shortest_edit_script(a, b, 0, 0)
-    assert got == want
-
-
-def test_ses_linestring_popnpushmiddle_3_string():
-    a = [(1, 1), (2, 2), (3, 3)]
-    b = [(1, 1), (4, 4), (3, 3)]
-    want = [(1, "delete"), (1, "insert", (4, 4))]
-    got = geodiff._shortest_edit_script(a, b, 0, 0)
-    assert got == want
-
-
-def test_ses_linestring_pop_3_string():
-    a = [(1, 1), (2, 2), (3, 3)]
-    b = [(1, 1), (2, 2)]
-    want = [(2, "delete")]
-    got = geodiff._shortest_edit_script(a, b, 0, 0)
-    assert got == want
-
-
-def test_ses_linestring_push_3_string():
-    a = [(1, 1), (2, 2)]
-    b = [(1, 1), (2, 2), (3, 3)]
-    want = [(1, "insert", (3, 3))]
-    got = geodiff._shortest_edit_script(a, b, 0, 0)
-    assert got == want
-
-
-def test_ses_linestring_popnpush_3_string():
-    a = [(1, 1), (2, 2), (3, 3)]
-    b = [(1, 1), (2, 2), (4, 4)]
-    want = [(2, "delete"), (2, "insert", (4, 4))]
-    got = geodiff._shortest_edit_script(a, b, 0, 0)
-    assert got == want
-
-
-def test_ses_linestring_popnchange_2_string():
-    a = [(1, 1), (2, 2)]
-    b = [(3, 3)]
-    want = [(0, "delete"), (0, "insert", (3, 3)), (1, "delete")]
-    got = geodiff._shortest_edit_script(a, b, 0, 0)
-    assert got == want
-
-
-def test_ses_linestring_example_from_article():
-    # abcabba -> cbabac
-    a = [(1, 1), (2, 2), (3, 3), (1, 1), (2, 2), (2, 2), (1, 1)]
-    b = [(3, 3), (2, 2), (1, 1), (2, 2), (1, 1), (3, 3)]
-    want = [
-        (0, "delete"),
-        (0, "insert", (3, 3)),
-        (2, "delete"),
-        (5, "delete"),
-        (6, "insert", (3, 3)),
-    ]
-    got = geodiff._shortest_edit_script(a, b, 0, 0)
-    assert got == want
-
-
-def test_ses_linestring_example1():
-    a = [(1, 1), (2, 2), (3, 3), (4, 4)]
-    b = [(2, 2), (3, 3)]
-    want = [(0, "delete"), (3, "delete")]
-    got = geodiff._shortest_edit_script(a, b, 0, 0)
-    assert got == want
-
-
-def test_ses_linestring_example2():
-    a = [(1, 1), (2, 2), (3, 3), (4, 4), (5, 5)]
-    b = [(8, 8), (9, 9), (3, 3), (4, 4)]
-    want = [
-        (-1, "insert", (8, 8)),
-        (-1, "insert", (9, 9)),
-        (0, "delete"),
-        (1, "delete"),
-        (4, "delete"),
-    ]
-    got = geodiff._shortest_edit_script(a, b, 0, 0)
-    assert got == want
-
-
-def test_ses_linestring_example3():
-    a = [(1, 1), (2, 2), (3, 3), (4, 4)]
-    b = [(2, 2), (8, 8), (9, 9), (3, 3)]
-    want = [
-        (0, "delete"),
-        (1, "insert", (8, 8)),
-        (1, "insert", (9, 9)),
-        (3, "delete"),
-    ]
+@pytest.mark.parametrize("scenario", scenarios, ids=idfn)
+def test_shortest_edit_script(scenario: Scenario):
+    a, b = scenario[1]
+    want = scenario[2]
     got = geodiff._shortest_edit_script(a, b, 0, 0)
     assert got == want
