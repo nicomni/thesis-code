8f65a4b63786a3628eae42804b5e14e23defc9fe
Nicolaj Magga Nielsen <nicolaj.niels1@gmail.com>
2024-02-23 13:51:40 +0100

    chore(format): format file

--- src/thesis/geodiff/errors.py
+++ src/thesis/geodiff/errors.py
@@ -2,12 +2,10 @@ from shapely.geometry.base import BaseGeometry
 
 
 class GeometryTypeMismatchError(Exception):
-
     def __init__(self, geom1: BaseGeometry, geom2: BaseGeometry):
         self._geom1 = geom1
         self._geom2 = geom2
         super().__init__()
-
 
     def __str__(self):
         return f"Geometry type mismatch: {self._geom1.geom_type} != {self._geom2.geom_type}"
