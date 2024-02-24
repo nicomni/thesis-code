09f81b7a22ffc477e52063329dea378e9dd8776f
Nicolaj Magga Nielsen <nicolaj.niels1@gmail.com>
2024-02-24 17:53:38 +0100

    feat: yield changed nodes from osc file

--- src/thesis/api/osc.py
+++ src/thesis/api/osc.py
@@ -1,27 +1,18 @@
 import xml.etree.ElementTree as ET
 
-from thesis.osm import (ChangeInfo, ChangeType, ElementIdentifier, ElementType,
-                        OSCInfo)
+from thesis.osm import ChangeType
+
+NODE_ELEMENT_TAG = "node"
 
 
-def get_change_info(osc_file_path: str) -> OSCInfo:
-    """Get change info from osc file."""
+def changed_nodes(osc_file_path: str):
     tree = ET.parse(osc_file_path)
     root = tree.getroot()
-    result: OSCInfo = []
-    for change_tag in root:
-        info = _get_change_info_for_group(change_tag)
-        result.extend(info)
-    return result
 
-
-def _get_change_info_for_group(change_tag: ET.Element) -> OSCInfo:
-    result = []
-    change_type = ChangeType[change_tag.tag.upper()]
-    for element_tag in change_tag:
-        entity_id = int(element_tag.attrib["id"])
-        idf = ElementIdentifier(entity_id, ElementType[element_tag.tag.upper()])
-        info = ChangeInfo(change_type, idf)
-        result.append(info)
-
-    return result
+    for change in root:
+        change_type = ChangeType[change.tag.upper()]
+        for element in change:
+            if element.tag != NODE_ELEMENT_TAG:
+                continue
+            node_id = int(element.attrib["id"])
+            yield (node_id, change_type)
