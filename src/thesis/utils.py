f4cddd43db1eb7fd1bcd3bff2c2317fd1983c76b
Nicolaj Magga Nielsen <nicolaj.niels1@gmail.com>
2024-02-25 17:22:50 +0100

    refactor: remove old code

--- src/thesis/utils.py
+++ src/thesis/utils.py
@@ -1,8 +1,6 @@
 import json
 
 from osgeo import ogr
-from shapely import GeometryType, LineString, Point, from_wkt, get_type_id
-from shapely.geometry.base import BaseGeometry
 
 from thesis import geo, gisevents
 from thesis.geodiff import geodiff
@@ -46,75 +44,6 @@ def to_lspatch_message(patch: geodiff.LSPatch) -> gisevents.LineStringPatch:
 
     result = gisevents.LineStringPatch(command=command, index=index, vector=vector)
     return result
-
-
-def get_pointdiff_message(a: Point, b: Point) -> gisevents.Point:
-    """Calculate the difference between two points.
-
-    RETURNS:
-        A protocol buffer Point message.
-    """
-    pointdiff = geodiff.diff_points(a, b)
-    lat = pointdiff[0]
-    lon = pointdiff[1]
-    ilat = geo.to100nano(lat)
-    ilon = geo.to100nano(lon)
-    return gisevents.Point(lat=ilat, lon=ilon)
-
-
-def get_linediff_message(a: LineString, b: LineString):
-    patch = geodiff.diff_linestrings(a, b)
-    N = len(patch)
-    indeces = [0] * N
-    commands = [gisevents.LineStringPatch.Command.CHANGE] * N
-    diff_vectors = [gisevents.Point()] * N
-    for i, cmd in enumerate(patch):
-        index = cmd[0]
-        indeces[i] = index
-        name = cmd[1]
-        if len(cmd) == 3:
-            # Is insert or change command
-            point = cmd[2]
-            ilat, ilon = (geo.to100nano(point[0]), geo.to100nano(point[1]))
-            diff_vector = gisevents.Point(lat=ilat, lon=ilon)
-            diff_vectors[i] = diff_vector
-
-        match name:
-            case "insert":
-                commands[i] = gisevents.LineStringPatch.Command.INSERT
-            case "change":
-                commands[i] = gisevents.LineStringPatch.Command.CHANGE
-            case "delete":
-                commands[i] = gisevents.LineStringPatch.Command.DELETE
-            case _:
-                raise ValueError(f"Unexpected command name: {name}")
-
-    return gisevents.LineStringPatch(
-        index=indeces, command=commands, vector=diff_vectors
-    )
-
-
-# TODO: Refactor
-def get_geom_patch(prev_feature: ogr.Feature, curr_feature: ogr.Feature):
-    # Check geometry types
-    if (
-        prev_feature.GetGeometryRef().GetGeometryType()
-        != curr_feature.GetGeometryRef().GetGeometryType()
-    ):
-        raise TypeError("Geometry type mismatch.")
-    # Check for geometry change
-    prev_geom: BaseGeometry = from_wkt(prev_feature.GetGeometryRef().ExportToWkt())
-    curr_geom: BaseGeometry = from_wkt(curr_feature.GetGeometryRef().ExportToWkt())
-    if prev_geom.equals_exact(curr_geom, tolerance=1e-7):
-        return None
-    geom_type_id = get_type_id(prev_geom)
-    match geom_type_id:
-        case GeometryType.POINT:
-            return get_pointdiff_message(prev_geom, curr_geom)
-        case GeometryType.LINESTRING:
-            return get_linediff_message(prev_geom, curr_geom)
-        case _:
-            raise TypeError(f"Unsupported geometry type: {prev_geom.geom_type}.")
 
 
 def get_prop_patch(prev: ogr.Feature, curr: ogr.Feature):
