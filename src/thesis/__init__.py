b584742158cef08cba92eb3ad29faf81a6fa2009
Nicolaj Magga Nielsen <nicolaj.niels1@gmail.com>
2024-03-04 14:46:28 +0100

    feat(logging): set up logging when import package

--- src/thesis/__init__.py
+++ src/thesis/__init__.py
@@ -1,3 +1,11 @@
 from osgeo import ogr as _ogr
+import logging
 
 _ogr.UseExceptions()
+
+logging.basicConfig(
+    filename="thesis.log",
+    encoding="utf-8",
+    level=logging.DEBUG,
+    format="%(asctime)s:%(levelname)s: %(message)s",
+)
